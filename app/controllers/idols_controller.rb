de79d874b18062aa4d38c4cf9644edbb8e51a525
Bruno Allevato <brunoallevato@gmail.com>
2022-10-29 17:44:46 +0200

    finished idols actions and created views

--- app/controllers/idols_controller.rb
+++ app/controllers/idols_controller.rb
@@ -1,2 +1,39 @@
 class IdolsController < ApplicationController
+  before_action :set_idol, only: %i[show destroy]
+
+  def index
+    @idols = Idol.all
+  end
+
+  def show
+    # @idol = Idol.find(params[:id])
+  end
+
+  def new
+    @idol = Idol.new
+  end
+
+  def create
+    @idol = Idol.new(idol_params)
+    if @idol.save
+      redirect_to idol_path(@idol)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @idol.destroy
+    redirect_to idols_path
+  end
+
+  private
+
+  def idol_params
+    params.require(:idol).permit(:name, :power)
+  end
+
+  def set_idol
+    @idol = Idol.find(params[:id])
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :idol
+
+  validates :price
+  validates :idol_id
 end
