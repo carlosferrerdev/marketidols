9125c1657bb88a4160d6ed127c3a2bbc145a8942
Carlos Ferrer <carlosvferrer@gmail.com>
2022-10-31 20:03:32 -0300

    config map_controller to work with map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -7,6 +7,7 @@ export default class extends Controller {
     markers: Array
   }
 
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
@@ -14,5 +15,24 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
   }
+
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
+
 }
