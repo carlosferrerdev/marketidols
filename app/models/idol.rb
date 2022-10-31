abc051ab4c399bd2c5344bbf68c91fb0e21df8ac
Carlos Ferrer <carlosvferrer@gmail.com>
2022-10-31 19:46:45 -0300

    Configurating the MAPBOX API

--- app/models/idol.rb
+++ app/models/idol.rb
@@ -1,4 +1,7 @@
 class Idol < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   belongs_to :user
   has_many :orders, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_221024) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_213853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_221024) do
     t.text "description"
     t.float "latitude"
     t.float "longitude"
-    t.string "address"
     t.index ["user_id"], name: "index_idols_on_user_id"
   end
 
